63fe4f03557a95af54b84fcdacddf77e215bf7de
borzfele <borzfele@gmail.com>
2020-11-27 19:45:27 +0100

    added routing, material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShoppingListPageComponent } from './pages/shopping-list-page/shopping-list-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/shopping-list', pathMatch: 'full'},
+  {path: 'shopping-list', component: ShoppingListPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { ShoppingListPageComponent } from './pages/shopping-list-page/shopping-list-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
